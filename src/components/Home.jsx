3f5cf5849a4ef34730ab0c89347f9d35f3db8eae
Kunal Dangi <kunaldangi252@gmail.com>
2023-09-10 12:53:58 +0530

    readme
    - Added New Note component
    - Save and Delete Note added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Note from "./Note";
 import "./Home.css";
 
 
-
 function Home() {
+    const [notes, setNotes] = useState([]);
+
+    async function fetchNotes() {
+        console.log("Getting notes....")
+        try {
+            const response = await fetch("http://localhost:5888/api/notes/get", {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    login_token: localStorage.getItem('login_token')
+                })
+            });
+    
+            const data = await response.json();
+            setNotes(data);
+            console.log(data);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    function onclick_createNote() {
+        console.log("Clicked!")
+    }
+
+    const updateNotes = (newNotes) => {
+        setNotes(newNotes);
+    };
+
     return (<>
         <nav>
             <div className="icon-notes">
@@ -15,12 +50,14 @@ function Home() {
             </div>
 
         </nav>
-        <div className="note-add">
+        <div className="note-add" onClick={()=>{onclick_createNote()}}>
             Take a note...
         </div>
 
-        <div className="notes">
-            <Note/>
+        <div className="notes" >
+            {notes.map((note, index) => (
+                <Note key={index} title={note.title} content={note.content} _id={note._id} updateNotes={updateNotes} />
+            ))}
         </div>
     </>);
 }
